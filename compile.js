8a85474047c3f74bf51c469b4fbf4dd65c4db91b
mrkai1253 <mrkai1253@gmail.com>
2018-10-07 10:42:00 +0530

    Deployjs added

--- compile.js
+++ compile.js
@@ -6,6 +6,6 @@ const solc = require('solc'); //The solidity compiler
 //for different os
 const inboxPath = path.resolve(__dirname,'contracts','inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf8');
-console.log(solc.compile(source,1));
+
 
 module.exports = solc.compile(source, 1).contracts[':Inbox']; // 1 for the number of contracts
